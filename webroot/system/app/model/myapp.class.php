c020724ed071e82518b3244cba906286562fe6e2
Kevin Yu <kevin@lxkevin.(none)>
2011-01-19 13:42:54 -0800

    adding skeleton of myapp

---
+++ webroot/system/app/model/myapp.class.php
@@ -0,0 +1,21 @@
+<?php
+/** My first application module  **/
+class Myapp extends App
+{
+	function initial()
+	{
+		$this->db = $this->load_db($this->conf['dsn']['default']);
+	}
+    function session_auth()
+	{
+	}
+	function user_proc($userid, $param=null)
+	{
+		if (!$userid) return null;
+		return $this->db->table_proc($this->mytables['user'], $userid, $param);
+	}
+	function user_search($filter, $suffix=null, $fields='*')
+	{
+		return $this->db->table_search($this->mytables['user'], $filter, $suffix, $fields='*');
+	}
+}
