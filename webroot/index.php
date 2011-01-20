34bd76436ecc45d2a56c19794bd59bcac6201799
Kevin Yu <kevin@lxkevin.(none)>
2011-01-20 12:01:23 -0800

    move key constants definition to the index.php

--- webroot/index.php
+++ webroot/index.php
@@ -1,3 +1,6 @@
 <?php
-define('APP_DIR', dirname(__FILE__).'/system/app');
+define('SYS_DIR',  dirname(__FILE__).'/system');
+define('CORE_DIR', SYS_DIR.'/core');
+define('APP_DIR',  SYS_DIR.'/app');
+
 include(APP_DIR.'/controller/app.php');
