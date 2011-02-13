4e5ab428aa0c6fc9b9eb130d130b5ef0f21d2ff7
Kevin Q. Yu <kevin@w3softwares.com>
2011-02-13 14:08:20 -0800

    Add default model for root entry in conf

--- webroot/system/app/conf/database.php
+++ webroot/system/app/conf/database.php
@@ -4,7 +4,7 @@ define('TZ_OFFSET', 0); // GMT
 define('SESSIONLIFETIME', 3600*24);
 define('SESSION', $_SERVER['HTTP_HOST']);
 
-$conf = array();
+$conf = array('model'=>null); // application model for root entry
 
 $dsn['default']['hostname'] = '';
 $dsn['default']['username'] = '';
--- webroot/system/app/controller/app.php
+++ webroot/system/app/controller/app.php
@@ -15,14 +15,6 @@ require_once(CORE_DIR."/controller.class.php");
 
 Class AppController extends Controller
 {
-	function initial()
-	{
-		if ($this->request('_ENTRY')=='/'||!$this->request('_ENTRY')) {
-			// set up application model for root entry
-			# $this->map['model'] = 'your_main_model';
-			$this->output($this->load_view('welcome'));
-		}
-	}
 }
 $app = new AppController;
 $app->boot();
