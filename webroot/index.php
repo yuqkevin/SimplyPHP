aa7bd146051944734257d75b9ed675a7be4f07f5
Kevin Q. Yu <kevin@w3softwares.com>
2012-07-24 10:35:51 -0700

    Re-define global folder constants for easy re-locating

--- webroot/index.php
+++ webroot/index.php
@@ -1,6 +1,7 @@
 <?php
+define('VERSION', 'v0.9');
 define('ROOT_DIR', dirname(__FILE__));
-define('SYS_DIR',  ROOT_DIR.'/system');
-define('CORE_DIR', SYS_DIR.'/core');
-define('APP_DIR',  SYS_DIR.'/app');
-include(APP_DIR.'/controller/app.php');
+define('CORE_DIR', ROOT_DIR.'/system/core');
+define('APP_DIR',  ROOT_DIR.'/system/app');
+define('SRC_DIR',  APP_DIR.'/resource');
+include(CORE_DIR.'/controller.class.php');
